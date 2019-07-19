ab36943623b932928a322b3ca9d1b59f209a6a8f
Michal K <32823477+michalk0128@users.noreply.github.com>
2019-07-19 23:08:51 +0200

    Update sitemap.sh

--- sitemap.sh
+++ sitemap.sh
@@ -5,9 +5,9 @@ cp public_html/sitemap.xml public_html/tmpsitemap.xml
 for file in $(while read line; do echo $line | grep "example.com" | cut -f4- -d/ | cut -f1 -d\<; done < public_html/tmpsitemap.xml); do
         DATE_FILE=$(ls -l --time-style=full-iso public_html/$(echo $file | sed 's/\/$/\/index/g').php | awk '{print $6}')
         DATE_FILE=$(echo ${DATE_FILE} | sed 's/\-/\\\-/g') && echo ${DATE_FILE}
-        SITEMAP_DATE_A=$(cat public_html/tmpsitemap.xml | grep "example.com/${file}<"| sed 's/\//\\\//g' | sed 's/\./\\\./g' | sed 's/\-/\\\-/g')
-        SITEMAP_DATE_B=$(cat public_html/tmpsitemap.xml | grep "example.com/${file}<" | sed "s/20[0-9]*-[0-9]*-[0-9]*/${DATE_FILE}/g" | sed 's/\//\\\//g' | sed 's/\./\\\./g' | sed 's/\-/\\\-/g')
-        sed -i "s/${SITEMAP_DATE_A}/${SITEMAP_DATE_B}/g" public_html/tmpsitemap.xml && echo $file && echo "Old date: ${SITEMAP_DATE_A}" && echo "New date: ${SITEMAP_DATE_B}" && sleep 1
+        SITEMAP_ENTRY_A=$(cat public_html/tmpsitemap.xml | grep "example.com/${file}<"| sed 's/\//\\\//g' | sed 's/\./\\\./g' | sed 's/\-/\\\-/g')
+        SITEMAP_ENTRY_B=$(cat public_html/tmpsitemap.xml | grep "example.com/${file}<" | sed "s/20[0-9]*-[0-9]*-[0-9]*/${DATE_FILE}/g" | sed 's/\//\\\//g' | sed 's/\./\\\./g' | sed 's/\-/\\\-/g')
+        sed -i "s/${SITEMAP_ENTRY_A}/${SITEMAP_ENTRY_B}/g" public_html/tmpsitemap.xml && echo $file && echo "Old date: ${SITEMAP_ENTRY_A}" && echo "New date: ${SITEMAP_ENTRY_B}" && sleep 1
 done
 
 if [[ $(diff public_html/sitemap.xml public_html/tmpsitemap.xml) ]]; then
